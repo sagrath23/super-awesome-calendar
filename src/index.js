3fc39b9dc88d616ec668fd7ad7357e2e108ef2e6
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-07-11 00:31:19 -0500

    add basic redux setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
